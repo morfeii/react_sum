b537ce672acfc05cdf57a2f5f67a8c3a20f28d7d
morfeii <morfeii88@gmail.com>
2020-02-11 23:30:59 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0be2a4d51bf87742479c1b1baf41cd4d.js"
+  "./precache-manifest.5e31f04cd94b24119d9f0eb3e7557049.js"
 );
 
 self.addEventListener('message', (event) => {
